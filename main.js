9be1c08e7fb010a7326c5845f21cc376aa543fcb
undrbug <undrbug@gmail.com>
2022-03-26 13:04:22 -0300

    clase03 complete

--- main.js
+++ main.js
@@ -3,8 +3,21 @@ const btn = document.querySelector("[data-form-btn]");
 const createTask = (event) => {
     event.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    console.log("crear tarea", input.value);
+    const value = input.value;
+    const task = document.createElement("li");
+    task.classList.add("card")
+    console.log("crear tarea", value);
+    const list = document.querySelector("[data-list]");
     input.value = "";
+    //backticks para buscar 
+    const content =   `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>`;
+  task.innerHTML = content;
+
+  list.appendChild(task);
+
 }
 
 //console.log(input);
