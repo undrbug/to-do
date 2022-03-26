a688da597dee9cebb70ba57f29707fd9fe5e05af
undrbug <undrbug@gmail.com>
2022-03-26 15:11:06 -0300

    clase04 complete

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
+( () => { //IIFE expresión de función inmediatamente invocada. También conocido cómo función autoejecutable. Oculta las variables y funciones
 const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (event) => {
     event.preventDefault();
     const input = document.querySelector("[data-form-input]");
     const value = input.value;
+    const list = document.querySelector("[data-list]");
     const task = document.createElement("li");
     task.classList.add("card")
-    console.log("crear tarea", value);
-    const list = document.querySelector("[data-list]");
     input.value = "";
-    //backticks para buscar 
-    const content =   `<div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-  </div>`;
-  task.innerHTML = content;
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task")
+    titleTask.innerHTML = value;
+    taskContent.appendChild(titleTask);
 
-  list.appendChild(task);
-
+    //backticks para buscar es como la comilla invertida
+    const content = `
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    //task.innerHTML = content;
+    task.appendChild(taskContent);
+    list.appendChild(task);
 }
 
-//console.log(input);
-
 btn.addEventListener("click", createTask);
+
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far","fa-check-square","icon");
+    i.addEventListener("click", completeTask);
+    return i;
+}
+
+const completeTask = (event) => {
+    const element = event.target;
+    element.classList.toggle("fas"); //toggle cambia la clase (CSS) si es que es diferente.
+    element.classList.toggle("completeICon"); //solo puede ser una. 
+    element.classList.toggle("far"); //alterna entre una clase y la otra
+};
+
+}) ()
